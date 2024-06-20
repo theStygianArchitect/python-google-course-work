3d796773d1e29df5cd7522e11dc5a8fd43969cf9
Scott Hill <scott.work96@gmail.com>
2024-06-20 14:59:21 -0400

    can you check my answers and let me know what i need to fix

--- app/practice.py
+++ app/practice.py
@@ -1,6 +1,10 @@
-# Strings: ordered, immutable, text representation
-# %.format, f-Strings
-var = 3.6434673557
-var2 = 6
-my_string = f"the variable is {var*2 } and {var2}"
-print(my_string)
+from itertools import count, cycle, repeat
+add10 = lambda x:
+
+
+
+
+
+
+
+
